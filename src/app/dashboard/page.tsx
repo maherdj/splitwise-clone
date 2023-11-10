060453ff54d45a67ef3f6bb256987ce33bcd8377
Daniel Maher <danielmaher01@gmail.com>
2023-11-09 23:15:41 -0500

    working on popup to create new card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import type { NextPage } from "next";
 import Popup from "reactjs-popup";
-// ("use client");
 
 import React, { FC } from "react";
 
@@ -15,12 +16,30 @@ const Dashboard: NextPage = () => {
       <div className="m-32"></div>
       <div className="flex">
         <div className="flex-none w-20"></div>
-        <button
-          className="flex-none w-10 h-10 text-center border-2 "
-          // onClick={Card}
+        <Popup
+          trigger={
+            <button className="flex-none w-10 h-10 text-center border-2 ">
+              {" "}
+              +{" "}
+            </button>
+          }
+          modal
+          nested
         >
-          +
-        </button>
+          {/* (close () = ( */}
+          <div className="max-w-fit max-h-fit border wind">
+            {/* test */}
+            <div className="flex-auto px-4 py-2">
+              <div>test</div>
+              <input
+                className="flex_auto  text-black "
+                placeholder="test"
+              ></input>
+            </div>
+
+            <button onClick={() => close()}>Close</button>
+          </div>
+        </Popup>
         <h1 className="flex-auto w-50 text-center justify-center h-10">
           Group {/* PLACEHOLDER */}
         </h1>
